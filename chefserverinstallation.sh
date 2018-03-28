ad8a781b8c0d4b28536361633d279424fdba8dc3
Anand Bhupathiraju <anandbhupathiraju@icloud.com>
2018-03-28 14:36:46 +0530

    Create chefserverinstallation.sh

---
+++ chefserverinstallation.sh
@@ -0,0 +1,5 @@
+wget https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm
+wget https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm
+sudo rpm -ivh *
+sudo chef-server-ctl reconfigure
+sudo chef-manage-ctl reconfigure
