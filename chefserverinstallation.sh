1e54fad724edd35b754882b0c96797866d62c7e6
Anand Bhupathiraju <anandbhupathiraju@icloud.com>
2018-03-28 18:10:44 +0530

    Update chefserverinstallation.sh

--- chefserverinstallation.sh
+++ chefserverinstallation.sh
@@ -1,5 +1,6 @@
 wget https://packages.chef.io/files/stable/chef-server/12.17.33/el/7/chef-server-core-12.17.33-1.el7.x86_64.rpm
 wget https://packages.chef.io/files/stable/chef-manage/2.5.8/el/7/chef-manage-2.5.8-1.el7.x86_64.rpm
-sudo rpm -ivh *
+sudo rpm -ivh chef-server-core-12.17.33-1.el7.x86_64.rpm
+sudo rpm -ivh chef-manage-2.5.8-1.el7.x86_64.rpm
 sudo chef-server-ctl reconfigure
 sudo chef-manage-ctl reconfigure
